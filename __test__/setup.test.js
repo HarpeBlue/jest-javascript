24b2ce364980adca7e72f043c3abfcb75c9efa42
HarpeBlue <hspe2507@gmail.com>
2021-01-24 18:35:58 -0500

    feat: setup.test.js

---
+++ __test__/setup.test.js
@@ -0,0 +1,16 @@
+// después de cada prueba
+afterEach(() => {
+  console.log('Después de cada prueba')
+})
+afterAll(() => {
+  console.log('Después de todas las pruebas')
+})
+
+//antes de todas las pruebas
+beforeAll(() => {
+  console.log('Antes de todas las pruebas')
+})
+
+beforeEach(() => {
+  console.log('Antes de cada prueba')
+})
