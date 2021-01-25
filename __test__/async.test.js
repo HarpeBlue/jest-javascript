85bf8b8ca8a8a84d75fd7654a337fe8b8b6447b5
HarpeBlue <hspe2507@gmail.com>
2021-01-24 22:05:48 -0500

    feat: aplicaciones de testing para Async/Awaait

--- __test__/async.test.js
+++ __test__/async.test.js
@@ -1,7 +1,7 @@
 import { getDataFromApi } from '../promise'
 
 describe('Probar Async/Await', () => {
-  test('Realizar una petición a una api', async () => {
+  xtest('Realizar una petición a una api', async () => {
     const api = 'https://rickandmortyapi.com/api/character/';
     const getRick = 'https://rickandmortyapi.com/api/character/1';
 
@@ -13,5 +13,15 @@ describe('Probar Async/Await', () => {
       expect(data.name).toEqual('Rick Sanchez');
     });
 
+  });
+
+  xtest('Realizando una petición a una api con error', async () => {
+    const apiError = 'http://httpstat.us/404';
+    const peticion = getDataFromApi(apiError);
+    await expect(peticion).rejects.toEqual(Error('Request failed with status code 404'));
+  });
+
+  test('Resuelve un Hola! ', async () => {
+    await expect(Promise.resolve('Hola')).resolves.toBe('Hola');
   });
 })
